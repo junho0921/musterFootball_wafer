c7a793eb3815dad90f6fd4db6822b839757d1436
何家俊 <jiajunhe@kugou.net>
2018-09-03 11:09:35 +0800

    add: 数据库初始化

--- server/tools/cAuth.sql
+++ server/tools/cAuth.sql
@@ -34,4 +34,44 @@ CREATE TABLE `cSessionInfo` (
   KEY `skey` (`skey`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
 
+-- ----------------------------
+--  Table structure for `user_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info` (
+  `open_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_by` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `wx_img` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` int(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `match_tips` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `real_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cancel_muster_match` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `muster_match` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regret_join_match` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `join_match` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`open_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='项目用户信息';
+
+-- ----------------------------
+--  Table structure for `matchs`
+-- ----------------------------
+DROP TABLE IF EXISTS `matchs`;
+CREATE TABLE `matchs` (
+  `match_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_by` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `leader` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `max_numbers` int(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canceled` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canceled_reason` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `position` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `members` varchar(5000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`match_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='项目比赛信息';
+
 SET FOREIGN_KEY_CHECKS = 1;
