551cae4976b4128604e9643b39a57820eee9c53e
何家俊 <jiajunhe@kugou.net>
2018-09-16 19:48:47 +0800

    add limit

--- client/pages/musterlist/index.js
+++ client/pages/musterlist/index.js
@@ -5,7 +5,8 @@ let {
   showModel,
 } = require('../../common/tools.js');
 const {
-  statusName
+  statusName,
+  isJoinable
 } = require('../../common/const.js');
 
 Page({
@@ -17,30 +18,21 @@ Page({
   },
   onShow: function () {
     if (this.mid){
-      console.log('id', this.mid);
-      showBusy('加载信息中');
-      this.get(this.mid);
+      this.get();
     }else{
       showModel('没有id')
     }
   },
-  get: function(id){
+  get: function(){
+    let id = this.mid;
+    console.log('id', this.mid);
     console.log(';this.data', this.data)
-    REQ.getMatchInfo({
+    showBusy('加载信息中');
+    REQ.getMatchDetail({
       id: id.split(',')
     })
     .then(res => {
       wx.hideToast();
-      try {
-        res.forEach(item => {
-          item.statusName = statusName[item.status];
-        })
-        res.forEach(item => {
-          item.position = JSON.parse(item.position);
-        })
-      } catch (e) {
-        console.log('e', e);
-      }
       console.log('matchs', res);
       this.setData({ list: res });
       // 记录在全局，提供访问成员信息
@@ -48,5 +40,16 @@ Page({
     }, () => {
       showModel('获取比赛信息失败');
     })
-  }
+  },
+    shareMatch(){
+        wx.showShareMenu({
+            withShareTicket: true,
+            success: (e) => {
+                console.log('share success', e)
+            },
+            error: (e) => {
+                console.log('share error', e)
+            }
+        })
+    }
 });
