22a323d37670f6579ad26b43036c9c1555d5fbeb
何家俊 <jiajunhe@kugou.net>
2018-09-16 19:47:25 +0800

    add: register

--- client/pages/joinlist/index.js
+++ client/pages/joinlist/index.js
@@ -2,10 +2,12 @@ var REQ = require('../../common/request.js');
 let {
   showBusy,
   showSuccess,
-  showModel
+  showModel,
+  isNotRegister
 } = require('../../common/tools.js');
 const {
-  statusName
+  statusName,
+  isMatchUnfull
 } = require('../../common/const.js');
 
 Page({
@@ -13,38 +15,97 @@ Page({
     list: [],
   },
   onLoad: function (options) {
-    let id = options.id;
-    if (id) {
-      console.log('id', id);
-      showBusy('加载信息中');
-      this.get(id);
+    let {open_id} = global.userInfo;
+    if(!open_id){
+      return showModel('没有用户信息');
+    }
+    if(!options.id){
+      return showModel('没有比赛信息');
+    }
+    this.mid = options.id;
+    this.userId = open_id;
+  },
+  onShow: function () {
+    if (this.mid) {
+      this.get();
     } else {
-      showModel('没有id')
+      return showModel('没有比赛信息')
     }
   },
-  get: function (id) {
+  get: function () {
+    let id = this.mid;
+    console.log('id', id);
     console.log(';this.data', this.data)
-    REQ.getMatchInfo({
+    showBusy('加载信息中');
+    REQ.getMatchDetail({
       id: id.split(',')
     })
       .then(res => {
         wx.hideToast();
-        try {
-          res.forEach(item => {
-            item.statusName = statusName[item.status];
-          })
-          res.forEach(item => {
-            item.position = JSON.parse(item.position);
-          })
-        } catch (e) {
-          console.log('e', e);
-        }
         console.log('joinMatchs', res);
+        res.forEach(item => {
+          item.isJoined = item.members.includes(this.userId);
+        });
         this.setData({ list: res });
         // 记录在全局，提供访问成员信息
         global.joinMatchs = res;
       }, () => {
         showModel('获取比赛信息失败');
       })
-  }
+  },
+  requesting: false,
+  regretMatch: function(e){
+    // 检查是否已经注册
+    if(isNotRegister()){
+      return;
+    }
+    if(this.requesting){
+      return;
+    }
+    let { id } = e.currentTarget.dataset;
+    if (id) {
+      console.log('regretMatch', id);
+      showBusy('请求中');
+      this.requesting = true;
+      REQ.regretMatch({
+        id
+      }).then(res => {
+        this.requesting = true;
+        showSuccess('取消报名成功');
+        this.get();
+      }, res => {
+        this.requesting = true;
+        return showModel('error', '取消报名失败');
+      })
+    }else{
+      return showModel('error', '获取比赛信息失败');
+    }
+  },
+  joinMatch: function(e){
+    // 检查是否已经注册
+    if(isNotRegister()){
+      return;
+    }
+    if(this.requesting){
+      return;
+    }
+    let { id } = e.currentTarget.dataset;
+    if (id) {
+      console.log('joinMatch', id);
+      showBusy('请求中');
+      this.requesting = true;
+      REQ.joinMatch({
+        id
+      }).then(res => {
+        this.requesting = true;
+        showSuccess('取消报名成功');
+        this.get();
+      }, res => {
+        this.requesting = true;
+        return showModel('error', '取消报名失败');
+      })
+    }else{
+      return showModel('error', '获取比赛信息失败');
+    }
+  },
 });
